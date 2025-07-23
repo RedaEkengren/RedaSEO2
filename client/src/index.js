270471ca52a120c829663a042c7844b65108cd5f
RedaEkengren <redaekengren@protonmail.com>
2025-07-23 06:48:22 +0200

    Add files via upload

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,16 @@
- import React from 'react';
- import ReactDOM from 'react-dom/client';
--import './index.css';
--import App from './App';
-+import './index.css';
-+import App from './App';
-+import { BrowserRouter } from 'react-router-dom';
- import reportWebVitals from './reportWebVitals';
+// client/src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
  const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(
--  <React.StrictMode>
--    <App />
--  </React.StrictMode>
-+  <React.StrictMode>
-+    <BrowserRouter>
-+      <App />
-+    </BrowserRouter>
-+  </React.StrictMode>
- );
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>                            {/* ← wrap your App in BrowserRouter */}
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
